ae47a33bd77eaaa95fbded1615699d0fc6774d36
Minced Bash <linxuse@gmail.com>
2026-07-09 07:55:01 +0300

    hey

--- readygo.sh
+++ readygo.sh
@@ -4,20 +4,18 @@ if [[ "$USER" == root ]]; then
 	exit
 fi
 
-if [[ "$EUID" -ne 0 ]]; then
-	exec sudo "$0" "$@"
-fi
-
 echo "Upgrading and Installing required packages"
 sudo pacman -Syu $(cat pkgs.txt)
-
+sudo -k
 echo -e "\n\nCopying to .config"
 sleep 0.5
 cp -rv kitty nvim sway waybar ~/.config/
 cp -rv swaylock ~/.swaylock
 
 sudo cp -rv SilentSDDM /usr/share/sddm/themes/silentbeef
+sudo -k
 sudo cp -rv wallpaps /usr/share/background/beef
+sudo -k
 echo -e "\n\nProcess Complete, enjoy my theme."
 sudo -k
 sleep 2s && exit

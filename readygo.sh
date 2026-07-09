c781eca303df6d5b722f52444f9f2882839dd315
Minced Bash <linxuse@gmail.com>
2026-07-09 07:39:01 +0300

    fixed

--- readygo.sh
+++ readygo.sh
@@ -9,10 +9,10 @@ sudo pacman -Syu $(cat pkgs.txt)
 
 echo -e "\n\nCopying to .config"
 sleep 0.5
-mv -v kitty nvim sway waybar ~/.config/
-mv -v swaylock ~/.swaylock
+cp -rv kitty nvim sway waybar ~/.config/
+cp -rv swaylock ~/.swaylock
 
-sudo mv -v SilentSDDM /usr/share/sddm/themes/SilentSDDM
-sudo mv -v wallpaps /usr/share/background/beef
+sudo cp -rv SilentSDDM /usr/share/sddm/themes/silentbeef
+sudo cp -rv wallpaps /usr/share/background/beef
 echo -e "\n\nProcess Complete, enjoy my theme."
 sleep 2s && exit
